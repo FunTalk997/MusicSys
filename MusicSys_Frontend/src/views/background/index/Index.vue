34a243f38b421f2e7e8d12b4f981276d40f60a4a
DHTSANG <1215660566@qq.com>
2023-04-15 19:10:58 +0800

    pic

--- MusicSys_Frontend/src/views/background/index/Index.vue
+++ MusicSys_Frontend/src/views/background/index/Index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-const windowHeight = ref(0);
+// const windowHeight = ref(0);
 
-const updateWindowHeight = () => {
-  windowHeight.value = window.innerHeight;
-};
+// const updateWindowHeight = () => {
+//   windowHeight.value = window.innerHeight;
+// };
 
-window.addEventListener("resize", updateWindowHeight);
+// window.addEventListener("resize", updateWindowHeight);
 
-onMounted(() => {
-  windowHeight.value = window.innerHeight;
-});
+// onMounted(() => {
+//   windowHeight.value = window.innerHeight;
+// });
 </script>
 
 <template>
@@ -17,8 +17,12 @@ onMounted(() => {
     <template #header>
       <span>首页</span>
     </template>
-    <div class="hello" :style="`height: ${windowHeight - 200}px`">
+    <div class="hello">
       <h2>欢迎使用用 音乐后台管理系统</h2>
+      <div class="img">
+        <img class="pic" src="@/assets/pic1.jpg" />
+        <img class="pic" src="@/assets/pic2.jpg" />
+      </div>
     </div>
   </el-card>
 </template>
@@ -26,7 +30,16 @@ onMounted(() => {
 <style scoped lang="less">
 .hello {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+}
+
+.img {
+  display: flex;
+}
+
+.pic {
+  width: 50%;
+  height: 100%;
 }
 </style>
